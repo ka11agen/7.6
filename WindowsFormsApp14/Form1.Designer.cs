3f508cf21c0bf85dd6a45bb303f7f14ba7669cd7
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-06-03 20:31:53 +0700

    Рабочий конечный вариант

--- WindowsFormsApp14/Form1.Designer.cs
+++ WindowsFormsApp14/Form1.Designer.cs
@@ -30,29 +30,127 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
             // 
-            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(277, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 190);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Location = new System.Drawing.Point(591, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.CausesValidation = false;
+            this.textBox2.Location = new System.Drawing.Point(591, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(591, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Назначить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(656, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(588, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Время";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(591, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp14/Form1.cs
+++ WindowsFormsApp14/Form1.cs
@@ -16,8 +16,10 @@ namespace WindowsFormsApp14
         int[] y = new int[15] { 79, 69, 69, 59, 59, 59, 49, 49, 49, 49, 39, 39, 39, 39, 39 };
         int[] _x = new int[15] { 55, 50, 60, 45, 55, 65, 40, 50, 60, 70, 35, 45, 55, 65, 75 };
         int[] _y = new int[15] { 79, 69, 69, 59, 59, 59, 49, 49, 49, 49, 39, 39, 39, 39, 39 };
-        int[] x1= new int[15] { 45, 55, 65, 35, 75, 50, 40, 60, 70, 45, 55, 65, 50, 60, 55 };
+        int[] x1= new int[15] { 45, 55, 65, 35, 75, 50, 40, 60, 70, 55, 45, 65, 50, 60, 55 };
         int[] y1 = new int[15] { 170, 170, 170, 170, 170, 160, 160, 160, 160, 150, 150, 150, 140, 140, 130 };
+        int q = 1;
+        int MyTime=0;
         int z = 0;
         int k = 14;
         int c = 0;
@@ -39,7 +41,17 @@ namespace WindowsFormsApp14
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (k >= 0) { Refresh(); }
-            
+            else
+            {
+                for (int i=0; i<15; i++)
+                {
+                    x[i] = _x[i];
+                    y[i] = _y[i];  
+                }
+                z = 0;
+                k = 14;
+                c = 0;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,18 +61,21 @@ namespace WindowsFormsApp14
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
             
+        }
 
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
             g.DrawLine(pen, 0, 0, 120, 180);
             g.DrawLine(pen, 0, 180, 120, 0);
             g.DrawLine(pen, 0, 180, 120, 180);
             g.DrawLine(pen, 0, 0, 120, 0);
-            for (int i=0; i< 15; i++)
+            for (int i = 0; i < 15; i++)
             {
                 g.FillEllipse(brush, x[i], y[i], 10, 10);
             }
-            if (y[z] < y1[c]-10)
+            if (y[z] < y1[c] - 10)
             {
                 y[z] += _direction.Y;
             }
@@ -70,12 +85,53 @@ namespace WindowsFormsApp14
                 x[z] = x1[c];
                 x[k] = _x[0];
                 y[k] = _y[0];
-                z=k;
+                z = k;
                 k--;
                 c++;
             }
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int R = int.Parse(textBox1.Text);
+            int H = int.Parse(textBox2.Text);
+            this.pictureBox1.Location = new Point(R, H);
+            timer1.Start();
+            timer2.Start();
+            button2.Focus();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            MyTime++;
+            label1.Text = MyTime.ToString();
+
+        }
 
 
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (q == 1)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                q = 0;
+            }
+            else
+            {
+                timer1.Start();
+                timer2.Start();
+                q = 1;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (q == 1) { q = 0; }
+            timer1.Stop();
+            timer2.Stop();
 
         }
     }
